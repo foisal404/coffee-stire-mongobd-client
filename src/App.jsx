cb12b922388df6ced20565f6698fd703002b625c
foisal404 <foisalahmmed2@gmail.com>
2023-05-15 01:44:02 +0600

    basic mongodb post create using form data

---
+++ src/App.jsx
@@ -0,0 +1,151 @@
+import "./App.css";
+
+function App() {
+  const handlerForm=event=>{
+    event.preventDefault();
+    const form= event.target;
+    const name=form.name.value;
+    const chef=form.chef.value;
+    const photo=form.photo.value;
+    const taste=form.taste.value;
+    const suplier=form.suplier.value;
+    const details=form.details.value;
+    const category=form.category.value;
+    const coffee={name,chef,suplier,taste,category,details,photo}
+    console.log(coffee);
+    fetch('http://localhost:5000/coffees',{
+      method:"POST",
+      headers:{
+        "content-type":"application/json"
+      },
+      body: JSON.stringify(coffee)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+      if(acknowledged){
+        
+      }
+    })
+  }
+  return (
+    <>
+      <h2 className="text-center text-5xl">ADD New Coffee </h2>
+      <form className="w-full px-10 p-5" onSubmit={handlerForm}>
+        <div className="w-full px-10 p-5">
+          <div className="grid grid-cols-2  w-full">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Coffee Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="text"
+                name="name"
+                placeholder="Enter Coffee Name"
+              />
+            </div>
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                chef
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="text"
+                name="chef"
+                placeholder="Enter Coffee Chef"
+              />
+            </div>
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Suplier
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="text"
+                name="suplier"
+                placeholder="Enter Coffee Suplier"
+              />
+            </div>
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Taste
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="text"
+                name="taste"
+                placeholder="Enter Coffee Taste"
+              />
+            </div>
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Category
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="text"
+                name="category"
+                placeholder="Enter Coffee Category"
+              />
+            </div>
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Details
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                name="details"
+                type="text"
+                placeholder="Enter Coffee Details"
+              />
+            </div>
+
+          </div>
+          <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Photo Url
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password"
+                type="text"
+                name="photo"
+                placeholder="Enter Photo URL"
+              />
+            </div>
+            <button type="submit" className="btn btn-block p-3 px-12">Add Coffee</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App;
