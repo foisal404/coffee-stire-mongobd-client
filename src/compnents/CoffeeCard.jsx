af2d631a5464751b6911e5357b1e29cc9bac0a7d
foisal404 <foisalahmmed2@gmail.com>
2023-05-15 03:19:59 +0600

    coffee card delate btn connect with mongo DELETE method then add Main layout and navbar

--- src/compnents/CoffeeCard.jsx
+++ src/compnents/CoffeeCard.jsx
@@ -1,23 +1,61 @@
 import React from "react";
+import { FaEdit, FaRegEye, FaRegWindowClose } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const {_id, name, chef, suplier, taste, category, details, photo } = coffee;
+  const { _id, name, chef, suplier, taste, category, details, photo } = coffee;
+  const handlerDelete = (id) => {
+    // console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Ywant to delete this coffee!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffees/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.acknowledged > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
-      <div className="card card-side bg-base-100 shadow-xl">
+      <div className="card card-side bg-[#f5f4f171] shadow-xl">
         <figure>
-          <img
-            src={photo}
-            alt="Movie"
-          />
+          <img src={photo} alt="Movie" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{details}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
-            <button className="btn btn-warning">Update</button>
-            <button className="btn btn-error">dalete</button>
+          <div className="flex  items-center h-full">
+            <div className="flex-grow">
+              <h2 className="card-title">{name}</h2>
+              <p>{details}</p>
+              <p>{chef}</p>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-success">
+                <FaRegEye />
+              </button>
+              <button className="btn btn-dark">
+                <FaEdit />
+              </button>
+              <button
+                className="btn btn-error"
+                onClick={() => handlerDelete(_id)}
+              >
+                <FaRegWindowClose />
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Coffees from "../compnents/Coffees";
+import Main from "../compnents/Main/Main";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App></App>
-    },
-    {
-      path:"/coffees",
-      element:<Coffees></Coffees>,
-      loader:()=>fetch('http://localhost:5000/coffees'),
+      path:"/",
+      element:<Main></Main>,
+      children:[
+        {
+          path: "/",
+          element: <App></App>
+        },
+        {
+          path:"/coffees",
+          element:<Coffees></Coffees>,
+          loader:()=>fetch('http://localhost:5000/coffees'),
+        }
+      ]
     }
   ]);
 export default router
